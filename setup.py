9c38b1d97f9cca3e121b4fd8ed9496e471fb1140
B. van Lew <b.van_lew@lumc.nl>
2020-09-11 16:31:32 +0200

    Update version for testing

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, Extension
 setup(
     name="cibuildwheel_autopypi_example",
     ext_modules=[Extension('cibuildwheel_autopypi_example', sources=['cibuildwheel_autopypi_example.c'])],
-    version="0.1.19",
+    version="0.1.20.dev1",
 )
